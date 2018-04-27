c1939250f8cc0743d9cf76c87e56744c7355245c
Riuzan <ismael.aik@gmail.com>
2018-04-27 13:31:36 +0000

    Controladors i rutes en construcció, falta provar

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,18 @@ Rails.application.routes.draw do
   resources :votes
   resources :comments
   resources :events
-  resources :messages
-    root 'messages#new'
+  #  root 'events#new'
     
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    post '/users/:user_id/events', to: 'events#create'
+    post '/users/:user_id/events/:event_id', to: 'events#update'
+    post '/users/:user_id/events/:event_id', to: 'events#destroy'
+    
+    post '/users/:user_id/:event_id/comments', to: 'comments#create'
+    post '/users/:user_id/:event_id/comments/:id', to: 'comments#update'
+    post '/users/:user_id/:event_id/comments/:id', to: 'comments#destroy'
+    
+    post '/users/:user_id/events/:event_id/votes', to: 'votes#create'
+    post '/users/:user_id/events/:event_id/votes/:id', to: 'votes#update'
+    post '/users/:user_id/events/:event_id/votes/:id', to: 'votes#destroy'
+    
 end
--- test/controllers/foo_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class FooControllerTest < ActionDispatch::IntegrationTest
-  test "should get bar" do
-    get foo_bar_url
-    assert_response :success
-  end
-
-end
