a8796756cae2c515891045a6c051ac3872aaed69
Welcius <wepain@outlook.com>
2018-04-27 16:40:08 +0000

    Fixejant taules per a postgres (...)

--- db/migrate/20180424011352_add_faculty_ref_to_profile.rb
+++ db/migrate/20180424011352_add_faculty_ref_to_profile.rb
@@ -1,5 +1,5 @@
 class AddFacultyRefToProfile < ActiveRecord::Migration[5.1]
   def change
-    add_reference :profiles, :faculty, foreign_key => { :to_table => :locations }
+    add_reference :profiles, :faculty, foreign_key: { to_table: :locations }, index: true
   end
 end
