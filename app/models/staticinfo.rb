673599270ad987eb6d1e7537f0cd10965f57d556
patitosb <sara.bourjila@gmail.com>
2018-05-06 10:14:57 +0000

    info ordenada y guardada en arrays

--- app/models/staticinfo.rb
+++ app/models/staticinfo.rb
@@ -7,20 +7,56 @@ class StaticInfo
     base_uri "http://serveisoberts.gencat.cat"
     
     def info1
+          
         self.class.get('/equipaments/search?rows=ALL&categories.tema=recerca&categories.subtema=Estructures%20universitaries&categories.subtema2=Universitats&adreca.municipi=Barcelona')
     end
     
-    def info2
-        def var 
-             "UPC" 
-        end
+    def info2(var)
+      
         self.class.get('/equipaments/search?rows=ALL&adreca.municipi=Barcelona&categories.subtema=Facultats%20i%20escoles&categories.tema=Universitats&nickname='+var)
     end   
-    
-    
-    
-    
 end
 
-satatic_info = StaticInfo.new
-puts satatic_info.info2
+   def satatic_info 
+       StaticInfo.new
+   end  
+   
+body = JSON.parse(satatic_info.info1.body)
+
+#universidades y su info
+ universidades_json = []
+ facultades_json = []
+
+body["items"].each do |doc|
+    universidades_json << doc["id"].to_json
+    universidades_json << doc["nickname"].to_json
+    universidades_json << doc["titol"].to_json
+    universidades_json << doc["adreca"].to_json
+    universidades_json << doc["url"].to_json
+    universidades_json << doc["telefons"].to_json
+    universidades_json << doc["fax"].to_json
+    universidades_json << doc["geolocalitzacio"].to_json
+
+    body2 = JSON.parse(satatic_info.info2(doc["nickname"]).body)  
+    
+    body2["items"].each do |doc2|
+    
+    facultades_json << doc2["id"].to_json
+    facultades_json << doc2["titol"].to_json
+    facultades_json << doc2["adreca"].to_json
+    facultades_json << doc2["telefons"].to_json
+    facultades_json << doc2["geolocalitzacio"].to_json
+    
+  #  puts facultades_json
+
+ end
+
+    
+   # puts universidades_json
+end
+
+
+
+
+
+ 
